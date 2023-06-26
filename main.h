282c1232d54db08648e4411d9756554f36aebb8a
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-26 13:40:49 -0700

    function declarations

--- main.h
+++ main.h
@@ -7,6 +7,10 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_number(int num);
+int print_formatted_string(const char *format, va_list args);
+int handle_conversion_specifier(char specifier, va_list args);
+int print_string(char *str);
+int print_integer(int num);
 
 #endif /* MAIN_H */
 
