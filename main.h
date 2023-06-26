88b92aab71a5a137b2aae65b85f8683185fe71a1
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-26 14:24:01 -0700

    print unsigned int in binary

--- main.h
+++ main.h
@@ -7,10 +7,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_number(int num);
-int print_formatted_string(const char *format, va_list args);
-int handle_conversion_specifier(char specifier, va_list args);
-int print_string(char *str);
-int print_integer(int num);
+int print_binary(unsigned int num);
 
 #endif /* MAIN_H */
 
