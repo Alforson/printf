0733dd00919c9b7b0ea1b26e2a15214511b18dca
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-26 12:52:43 -0700

    error fixes

--- _printf.c
+++ _printf.c
@@ -1,30 +1,14 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * _printf - Produces output according to a format.
  * @format: Character string containing the format.
  *
  * Return: The number of characters printed.
-=======
- * _printf - Prints output according to a format
- * @format: The format string
- *
- * Return: The number of characters printed (excluding the null byte)
->>>>>>> 88bef66271eb05f12e33d0e77e148512365bc02c
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-<<<<<<< HEAD
-	va_start(args, format);
-
-	int printed_chars = 0;
-	char c;
-	char *s;
-
-	while (*format != '\0')
-=======
 	int count = 0;
 	char *str;
 	char c;
@@ -36,55 +20,11 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	while (*format)
->>>>>>> 88bef66271eb05f12e33d0e77e148512365bc02c
 	{
 		if (*format == '%')
 		{
-<<<<<<< HEAD
-			format++; /* Move to the next character after '%' */
-
-			/* Handle conversion specifiers */
-			switch (*format)
-			{
-				case 'c':
-					c = (char)va_arg(args, int);
-					putchar(c);
-					printed_chars++;
-					break;
-
-				case 's':
-					s = va_arg(args, char *);
-					while (*s != '\0')
-					{
-						putchar(*s);
-						s++;
-						printed_chars++;
-					}
-					break;
-
-				case '%':
-					putchar('%');
-					printed_chars++;
-					break;
-
-				default:
-					putchar('%');
-					putchar(*format);
-					printed_chars += 2;
-					break;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			printed_chars++;
-=======
-			_putchar(*format);
-			count++;
-		}
-		else
-		{
 			format++;
+
 			if (*format == '\0')
 				return (-1);
 
@@ -128,7 +68,11 @@ int _printf(const char *format, ...)
 				_putchar(*format);
 				count += 2;
 			}
->>>>>>> 88bef66271eb05f12e33d0e77e148512365bc02c
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
 		}
 
 		format++;
@@ -138,3 +82,4 @@ int _printf(const char *format, ...)
 
 	return (count);
 }
+
--- main.h
+++ main.h
@@ -6,11 +6,7 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-<<<<<<< HEAD
-int print_number(int n);
-=======
 int print_number(int num);
->>>>>>> 88bef66271eb05f12e33d0e77e148512365bc02c
 
 #endif /* MAIN_H */
 
